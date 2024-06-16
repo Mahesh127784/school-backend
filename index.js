14290d6563c76cee8a35b15536b833aadca9d692
GurudeepNaik <gurudeepnaik1999@gmail.com>
2024-06-16 12:42:40 +0530

    hello world

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // require("dotenv").config({ path: "./env" });
-import dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
+// import dotenv from "dotenv";
+// dotenv.config({ path: "./.env" });
 
 import app from "./src/app.js";
 
-import connectDB from "./src/db/index.js";
+// import connectDB from "./src/db/index.js";
 const port = process.env.PORT;
 
-connectDB()
-  .then(() => {
+// connectDB()
+  // .then(() => {
     app.listen(port || 8000, () => {
       console.log("Server is running at port : ", port);
     });
-  })
-  .catch((err) => {
-    console.log("Mongodb connection failed : ", err);
-  });
+  // })
+  // .catch((err) => {
+    // console.log("Mongodb connection failed : ", err);
+  // });
--- src/app.js
+++ src/app.js
@@ -1,39 +1,39 @@
 import express from "express";
-import cookieParser from "cookie-parser";
-import cors from "cors";
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.CORES_ORIGIN,
-    credentials: true,
-  })
-);
-app.use(express.json({ limit: "16kb" }));
-app.use(express.urlencoded({ extended: true, limit: "16kb" })); // telling server to understand the encoded data from an encoded url if the they are encoded
-app.use(express.static("public")); //to save/store files or images in public folder
-app.use(cookieParser()); //to access/set cookies from/in users browser
+// app.use(
+//   cors({
+//     origin: process.env.CORES_ORIGIN,
+//     credentials: true,
+//   })
+// );
+// app.use(express.json({ limit: "16kb" }));
+// app.use(express.urlencoded({ extended: true, limit: "16kb" })); // telling server to understand the encoded data from an encoded url if the they are encoded
+// app.use(express.static("public")); //to save/store files or images in public folder
+// app.use(cookieParser()); //to access/set cookies from/in users browser
 
-// routes import and declaration / available routes
-import adminRouter from "./routes/admin.router.js";
-app.use("/api/v1/admins", adminRouter);
+// // routes import and declaration / available routes
+// import adminRouter from "./routes/admin.router.js";
+// app.use("/api/v1/admins", adminRouter);
 
-import userRouter from "./routes/user.router.js";
-app.use("/api/v1/users", userRouter);
+// import userRouter from "./routes/user.router.js";
+// app.use("/api/v1/users", userRouter);
 
-import admissionRouter from "./routes/admission.router.js";
-app.use("/api/v1/admissions", admissionRouter);
+// import admissionRouter from "./routes/admission.router.js";
+// app.use("/api/v1/admissions", admissionRouter);
 
-import teacherRouter from "./routes/teacher.routes.js";
-app.use("/api/v1/teachers", teacherRouter);
+// import teacherRouter from "./routes/teacher.routes.js";
+// app.use("/api/v1/teachers", teacherRouter);
 
-import studentRouter from "./routes/student.router.js";
-app.use("/api/v1/students", studentRouter);
+// import studentRouter from "./routes/student.router.js";
+// app.use("/api/v1/students", studentRouter);
 
-app.get("/api/v1/reurnsuccess", (req, res) => {
-  res.send("successfull");
-});
+// app.get("/api/v1/reurnsuccess", (req, res) => {
+//   res.send("successfull");
+// });
 
 app.get("/", (req, res) => {
   res.send("Welcome to School Administration Backend!.");
